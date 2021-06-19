0e8cb04baf0f93c6d203b834b0ce62e06f8a4018
Vadym Shymko <vadymshymko@gmail.com>
2021-06-20 01:31:26 +0300

    Update packages

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
-    "@babel/plugin-transform-runtime",
-
     [
       "transform-react-remove-prop-types",
       {
